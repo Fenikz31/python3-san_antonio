358c78e5f6f3fdeb96b93544b379682e724c9d95
Fenikz31 <45063328+Fenikz31@users.noreply.github.com>
2018-11-15 10:04:29 +0100

    Add files via upload

---
+++ san_antonio.py
@@ -0,0 +1,53 @@
+# -*- coding: utf8 -*-
+import random
+import json
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chipmunks",
+    "Babar",
+    "betty boop",
+    "calimero",
+    "casper",
+    "le chat potté",
+    "Kirikou"
+]
+
+def read_values_from_json(characters, character):
+	values = []
+	with open("characters.json") as f:
+		data = json.load(f)
+		for entry in data:
+			values.append(entry["character"])
+	return values
+
+def random_character():
+	all_values = read_values_from_json()
+	return get_random_item_in(all_values)
+
+def message(character, quote):
+    n_character = character.capitalize()
+    n_quote = quote.capitalize()
+    return "{} a dit : {}".format(n_character, n_quote)
+
+def get_random_quote():
+    # get a random number
+    # get a quote from an array
+    # show the quote in the interpreter
+    pass
+
+def get_random_item_in(my_list):
+    rand_numb = random.randint(0, len(my_list) - 1)
+    item = my_list[rand_numb] # get a quote from a list
+    return item # return the item
+
+# Programm
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
+
+while user_answer != "B":
+    print(message(get_random_item_in(characters), get_random_item_in(quotes)))
+    user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
